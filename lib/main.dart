17902543c0f59f73f33a1d2a090ed061e80288e3
MOHAMEDESAMX <MOHAMEDESSAM.5530@GMAIL.COM>
2024-11-17 05:41:12 +0200

    main user state stream updated

--- lib/main.dart
+++ lib/main.dart
@@ -9,50 +9,52 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-  bool loged = false;
-void main() async{
+bool loged = false;
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
-  FirebaseAuth.instance.authStateChanges().listen((User? user) {
-    if (user == null) {
-      log('User is currently signed out!');
-      loged =false;
-    } else {
-      log('User is signed in!');
-      loged =true;
-    }
-  });
 }
-
-
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
-  
 }
 
 class _MyAppState extends State<MyApp> {
-  
+  @override
+  void initState() {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        log('User is currently signed out!');
+        loged = false;
+      } else {
+        log('User is signed in!');
+        loged = true;
+      }
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(360, 690),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (_ , child) {
-      return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.green,
-        ),
-        textTheme: GoogleFonts.interTextTheme(),
-      ),
-      home: (loged)? const HomePage() : const SplashScreen(),
-    );
-  });
-}}
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.green,
+              ),
+              textTheme: GoogleFonts.interTextTheme(),
+            ),
+            home: (loged) ? const HomePage() : const SplashScreen(),
+          );
+        });
+  }
+}
